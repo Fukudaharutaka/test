5e7ad1fe584c4b562ffd056d5311a83c58251299
Fukudaharutaka <vqnraikko@yahoo.co.jp>
2018-12-03 21:39:17 +0900

    aaaaa 20181203

---
+++ human/src/jp/co/internous/action/Human.java
@@ -0,0 +1,23 @@
+package jp.co.internous.action;
+
+public class Human {
+
+	public static void main(String[]args){
+
+
+		HumanName x =new HumanName();
+		System.out.println(x.getName());
+
+		HumanAge y = new HumanAge();
+		System.out.println(y.getAge());
+
+		HumanAddress z = new HumanAddress();
+		System.out.println(z.getAddress());
+
+
+
+
+
+	}
+
+}
